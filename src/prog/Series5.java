0d030b9d5e934c59be9236de1e439e852f6e9769
amarpreet <amarpreetsingh121@gmail.com>
2017-12-23 05:56:33 +0530

    first commit

---
+++ src/prog/Series5.java
@@ -0,0 +1,27 @@
+package prog;
+
+public class Series5 {
+//wap to print  0 1 1 2 3 5 8 13 21
+	public static void main(String args[])
+	{
+
+int a=0,b=1,c;
+
+
+for(int i=1;i<=9;i++)
+	{
+	System.out.print(a+" ");
+	c=a+b;
+	a=b;
+	b=c;
+	}
+
+
+
+
+
+}
+
+
+
+}
